48ca88782f00da0c7f643aa3e2d2e88f6a2a7f4d
Davis Phillips <dphillip@redhat.com>
2024-05-30 09:22:14 -0500

    Update smcBootFromCDFull.sh

--- smc/smcBootFromCDFull.sh
+++ smc/smcBootFromCDFull.sh
@@ -5,9 +5,13 @@ ISO="$3"
 
 curl --globoff  -L -w "%{http_code} %{url_effective}\\n" -ku ${USERNAME_PASSWORD} -H "Content-Type: application/json" -H "Accept: application/json" -d '{"ResetType": "ForceOff"}' -X POST https://$BMC_ADDRESS/redfish/v1/Systems/1/Actions/ComputerSystem.Reset
 
-curl --globoff  -L -w "%{http_code} %{url_effective}\\n" -ku ${USERNAME_PASSWORD} -H "Content-Type: application/json" -H "Accept: application/json" -d "{}" -X POST https://$BMC_ADDRESS/redfish/v1/Managers/1/VirtualMedia/VirtualMedia2/Actions/VirtualMedia.EjectMedia
+# older SMC curl --globoff  -L -w "%{http_code} %{url_effective}\\n" -ku ${USERNAME_PASSWORD} -H "Content-Type: application/json" -H "Accept: application/json" -d "{}" -X POST https://$BMC_ADDRESS/redfish/v1/Managers/1/VirtualMedia/VirtualMedia2/Actions/VirtualMedia.EjectMedia
+curl --globoff  -L -w "%{http_code} %{url_effective}\\n" -ku ${USERNAME_PASSWORD} -H "Content-Type: application/json" -H "Accept: application/json" -d "{}" -X POST https://$BMC_ADDRESS/curl --globoff  -L -w "%{http_code} %{url_effective}\\n" -ku ${USERNAME_PASSWORD} -H "Content-Type: application/json" -H "Accept: application/json" -d "{}" -X POST https://$BMC_ADDRESS/redfish/v1/Managers/1/VM1/CfgCD/Actions/IsoConfig.UnMount
 
-curl --globoff  -L -w "%{http_code} %{url_effective}\\n" -ku ${USERNAME_PASSWORD} -H "Content-Type: application/json" -H "Accept: application/json" -d "{"\"Image\"": "\"${ISO}\""}" -X POST https://$BMC_ADDRESS/redfish/v1/Managers/1/VirtualMedia/VirtualMedia2/Actions/VirtualMedia.InsertMedia
+
+# older SMC curl --globoff  -L -w "%{http_code} %{url_effective}\\n" -ku ${USERNAME_PASSWORD} -H "Content-Type: application/json" -H "Accept: application/json" -d "{"\"Image\"": "\"${ISO}\""}" -X POST https://$BMC_ADDRESS/redfish/v1/Managers/1/VirtualMedia/VirtualMedia2/Actions/VirtualMedia.InsertMedia
+
+curl --globoff  -L -w "%{http_code} %{url_effective}\\n" -ku ${USERNAME_PASSWORD} -H "Content-Type: application/json" -H "Accept: application/json" -d "{"\"Image\"": "\"${ISO}\""}" -X POST https://$BMC_ADDRESS//redfish/v1/Managers/1/VM1/CfgCD/Actions/IsoConfig.Mount
 
 curl --globoff  -L -w "%{http_code} %{url_effective}\\n"  -ku ${USERNAME_PASSWORD}  -H "Content-Type: application/json" -H "Accept: application/json" -d '{"Boot":{ "BootSourceOverrideEnabled": "Once", "BootSourceOverrideTarget": "Cd", "BootSourceOverrideMode": "UEFI"}}' -X PATCH https://$BMC_ADDRESS/redfish/v1/Systems/1
 
